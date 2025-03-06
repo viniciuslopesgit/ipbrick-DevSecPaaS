a3c42e9e8f561b336aa295306b2a329c9f26802a
vlopes <vlopes@ipbrick.com>
2025-03-06 16:30:15 +0000

    remove .share

--- .share/backup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#
-ORIG="/home/vlopes/Documentos"
-DEST="/run/user/1001/gvfs/smb-share:server=etlas.ipbrick.com,share=vlopes/Backups/Documentos"
-
-rsync -av --no-perms --delete "$ORIG/" "$DEST/"
